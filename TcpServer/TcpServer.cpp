a3104acfbb9a115f49d26ee4c05b3c1b57fc000d
MarkZhongsh <MarkZhongsh@gmail.com>
2016-05-31 16:42:17 +0800

    1. 修改获取socket逻辑; 2. 添加发送

--- TcpServer/TcpServer.cpp
+++ TcpServer/TcpServer.cpp
@@ -7,13 +7,13 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <string.h>
+#include <error.h>
 
 #include "TcpServer.h"
 
 #define MINPORT 1
 #define MAXPORT 65535
 #define BUFFER_SIZE 512
-
 
 
 TcpServer::TcpServer():port(0),maxConnect(10),server_fd(0)
@@ -55,10 +55,12 @@ void TcpServer::connect()
     //端口不能超过范围
     assert(this->port >= MINPORT && this->port < MAXPORT);
 
-    if(( this->server_fd = socket(AF_INET, SOCK_STREAM, 0)))
+    if(( this->server_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
     {
 #ifdef DEBUG
-        perror("bind 出错");
+        extern int error;
+        perror(strerror(error));
+        error = 0;
         exit(1);
 #endif
     }
@@ -112,13 +114,26 @@ void TcpServer::beginAccept()
     {
         memset(buffer, 0, sizeof(buffer));
         int len = recv(conn, buffer, sizeof(buffer), 0);
-        if(len != 0)
+        if(len > 0)
         {
             printf("%d\n", len);
             printf("%s\n", buffer);
+            
+            char sendStr[] = "asdf";
+            sprintf(sendStr, "%s_from_server", buffer);
+            printf("ready to send str: %s\n", sendStr);
+            int result = send(conn, sendStr, strlen(sendStr), MSG_DONTWAIT);
+            if(result < 0)
+            {
+                printf("send error:%d\n", result);
+                perror("发送失败");
+            }
+
         }
     }
+    printf("exit accept\n");
     close(conn);
+    close(this->server_fd);
 }
 
 
